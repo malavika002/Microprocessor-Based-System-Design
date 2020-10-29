60b3908b39ef58454965dd711849d1782fe078dd
Malavika <61610626+malavika002@users.noreply.github.com>
2020-10-29 11:14:21 +0800

    Add files via upload

---
+++ Switches_LED/Debug/subdir_vars.mk
@@ -0,0 +1,59 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+Y:/CE2007Labs-Students-17072019/CE2007-Students/inc/Clock.c \
+Y:/CE2007Labs-Students-17072019/CE2007-Students/inc/CortexM.c \
+../Lab08_LED_Switchesmain.c \
+Y:/CE2007Labs-Students-17072019/CE2007-Students/inc/TExaS.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Clock.d \
+./CortexM.d \
+./Lab08_LED_Switchesmain.d \
+./TExaS.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Clock.obj \
+./CortexM.obj \
+./Lab08_LED_Switchesmain.obj \
+./TExaS.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Clock.obj" \
+"CortexM.obj" \
+"Lab08_LED_Switchesmain.obj" \
+"TExaS.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Clock.d" \
+"CortexM.d" \
+"Lab08_LED_Switchesmain.d" \
+"TExaS.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"Y:/CE2007Labs-Students-17072019/CE2007-Students/inc/Clock.c" \
+"Y:/CE2007Labs-Students-17072019/CE2007-Students/inc/CortexM.c" \
+"../Lab08_LED_Switchesmain.c" \
+"Y:/CE2007Labs-Students-17072019/CE2007-Students/inc/TExaS.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
